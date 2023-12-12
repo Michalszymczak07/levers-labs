d2b58cae9f4f6cb4d6ebcdd4764ce59708bddd24
Michalszymczak07 <86527270+Michalszymczak07@users.noreply.github.com>
2023-12-12 12:01:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.3/gsap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.3/ScrollTrigger.min.js"></script>
+
 
 <script>
 // NAV SHOW HIDE
