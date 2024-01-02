8dea012c4da2eee4536dc4ae2bff402a732810a6
Michalszymczak07 <86527270+Michalszymczak07@users.noreply.github.com>
2024-01-02 16:15:17 +0100

    Update and rename helpmeman4.js to helpmeman5.js

--- helpmeman5.js
+++ helpmeman5.js
@@ -36,7 +36,7 @@ function changeLetterColorsToBlack() {
   for (let i = 1; i <= 12; i++) {
     let letter = document.querySelector('#letter' + i);
     if (letter) {
-      letter.style.fill = '#181715'; // Change fill color to black
+      letter.style.fill = 'black'; // Change fill color to black
     }
   }
 }
