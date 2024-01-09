6ed514f0e7ea19a25078c4a87a6a8328a4e5584a
Michalszymczak07 <86527270+Michalszymczak07@users.noreply.github.com>
2024-01-09 19:56:40 +0100

    Update and rename homepage5.js to homepage6.js

--- homepage6.js
+++ homepage6.js
@@ -219,10 +219,13 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 
+gsap.registerPlugin(ScrollTrigger);
+
+// Animation for changing styles and animating state-4
 ScrollTrigger.create({
-    trigger: '#sp4', // Assuming #sp4 is the last state
-    start: 'top center', // Starts when the top of #sp4 hits the center of the viewport
-    end: 'bottom top', // Adjust as needed
+    trigger: '#sp4',
+    start: 'top center',
+    end: 'bottom top',
     onEnter: () => {
         gsap.to('#sp4-wrapper', { backgroundColor: '#181715', duration: 1 });
         gsap.to('#sp4', { color: 'white', duration: 1 });
@@ -240,7 +243,17 @@ ScrollTrigger.create({
                     width: '100%',
                     duration: 1,
                     stagger: 0.1, // slight delay between each orange element's animation
-                    ease: 'none'
+                    ease: 'none',
+                    onComplete: () => {
+                        // Pin .fixed-hero-wrapper after the animation completes
+                        ScrollTrigger.create({
+                            trigger: '.fixed-hero-wrapper',
+                            start: 'top top', // Start pinning when it's at the top
+                            end: '+=100vh', // Extend for an additional 100vh
+                            pin: true,
+                            pinSpacing: false
+                        });
+                    }
                 });
             }
         });
@@ -249,14 +262,11 @@ ScrollTrigger.create({
         gsap.to('#sp4-wrapper', { backgroundColor: '#E8DACC', duration: 1 });
         gsap.to('#sp4', { color: '', duration: 1 });
         gsap.to('.line-mask', { backgroundColor: '#E8DACC', duration: 1 });
-
-        // Revert state-4-line elements width to 0
         gsap.to('.state-4-line', { width: '0%', duration: 1, ease: 'none' });
-
-        // Also revert state-4-orange elements width to 0
         gsap.to('.state-4-orange', { width: '0%', duration: 1, ease: 'none' });
     }
 });
+
 }
 let mm = gsap.matchMedia();
 mm.add("(min-width: 768px)", () => {
@@ -264,5 +274,4 @@ mm.add("(min-width: 768px)", () => {
 });
 
 // THE CRAZY SECTION END>
-
 
